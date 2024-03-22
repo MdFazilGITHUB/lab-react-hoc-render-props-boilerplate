47b20394e26ab64245183404a25bb6323a2888f0
MdFazilGITHUB <mjam7221jmi@gmail.com>
2024-03-22 10:29:55 +0530

    done

---
+++ src/concepts/HigherOrderComponent/WithLoader.jsx
@@ -0,0 +1,17 @@
+import React, { useState } from "react";
+
+const WithLoader = (ChildComponent) => {
+  return (props) => {
+    const [loader, setLoader] = useState(true);
+
+    setTimeout(() => {
+      setLoader(false);
+    }, 2000);
+
+    return (
+      <>{loader ? <h1>Loading.....</h1> : <ChildComponent {...props} />}</>
+    );
+  };
+};
+
+export default WithLoader;
---
+++ src/concepts/render/ParentComponent.jsx
@@ -0,0 +1,11 @@
+import React, { useState } from "react";
+
+const Render = (props) => {
+  let [count, setCount] = useState(0);
+  const increase = () => {
+    setCount(count + 1);
+  };
+  return <>{props.render(increase, count)}</>;
+};
+
+export default Render;
